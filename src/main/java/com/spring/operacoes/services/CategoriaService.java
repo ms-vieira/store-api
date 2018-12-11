151500c931801f3843cebce77b340799370a3622
Matheus Silva <matheusv.silva@outlook.com>
2018-12-11 17:13:20 -0200

    Listando categorias com DTO.

--- src/main/java/com/spring/operacoes/services/CategoriaService.java
+++ src/main/java/com/spring/operacoes/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.spring.operacoes.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,5 +44,9 @@ public class CategoriaService {
 		catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui um produto!");
 		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
