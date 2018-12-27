a8a277a782b79ed8a902d54a9e8f3695d8112c51
Matheus Silva <matheusv.silva@outlook.com>
2018-12-27 16:44:56 -0200

    Correções update em categoria service.

--- src/main/java/com/spring/operacoes/services/CategoriaService.java
+++ src/main/java/com/spring/operacoes/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.spring.operacoes.domain.Categoria;
+import com.spring.operacoes.domain.Cliente;
 import com.spring.operacoes.dto.CategoriaDTO;
 import com.spring.operacoes.repositories.CategoriaRepository;
 import com.spring.operacoes.services.exceptions.DataIntegrityException;
@@ -34,8 +35,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	
@@ -62,5 +64,9 @@ public class CategoriaService {
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
