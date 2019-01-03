0eb1ae3d9e692d9699c76a22a251a4dd30cf135a
Matheus Silva <matheusv.silva@outlook.com>
2019-01-03 10:54:50 -0200

    Validação no e-mail.

--- src/main/java/com/spring/operacoes/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/spring/operacoes/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.spring.operacoes.domain.Cliente;
 import com.spring.operacoes.domain.enums.TipoCliente;
 import com.spring.operacoes.dto.ClienteNewDTO;
+import com.spring.operacoes.repositories.ClienteRepository;
 import com.spring.operacoes.resources.exception.FieldMessage;
 import com.spring.operacoes.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -27,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido!"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("Email", "E-mail já existente!"));
 		}
 		
 	// inclua os testes aqui, inserindo erros na lista
